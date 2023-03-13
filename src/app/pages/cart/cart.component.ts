5edea42d6c3a7ab3f819f1893c5b4457bdaed811
m-tonon <math.tonon@gmail.com>
2023-03-13 07:20:33 -0300

    feat: add cart component and build logic for material table

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html'
+})
+export class CartComponent implements OnInit{
+  cart: Cart = {items: [{
+    product: 'https://placehold.co/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 1,
+    id: 1
+  }] };
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ];
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+}
