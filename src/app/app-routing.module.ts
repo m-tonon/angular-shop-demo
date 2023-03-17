88835b16710103904439f3db08bbb160c336cc9c
m-tonon <math.tonon@gmail.com>
2023-03-17 06:48:01 -0300

    feat: add authentication page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { HomeComponent } from './pages/home/home.component';
 
@@ -10,6 +11,10 @@ const routes: Routes = [{
 {
   path: 'cart',
   component: CartComponent
+},
+{
+  path: 'auth',
+  component: AuthComponent
 },
 {
   path: '', redirectTo: 'home', pathMatch: 'full'
