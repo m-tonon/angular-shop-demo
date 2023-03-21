8a808b05e300eb6e5b2648d8fc45a97ca17fcd9e
m-tonon <math.tonon@gmail.com>
2023-03-21 17:47:26 -0300

    feat: create user data model to store core data and validate token

--- src/app/pages/auth/auth.service.ts
+++ src/app/pages/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
 
 import {environment} from '../../../../environments/environment';
+import { User } from "src/app/models/user.model";
 
 export interface AuthResponseData {
   idToken: string;
@@ -18,6 +19,8 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
+  user = new Subject<User>;
+
   constructor (private http: HttpClient) {}
 
   signup(_email: string, _password: string) {
@@ -28,7 +31,16 @@ export class AuthService {
         password: _password,
         returnSecureToken: true
       }
-    ).pipe(catchError(this.handleError));
+    ).pipe(catchError(this.handleError),
+    tap((resData) => {
+      this.handleAuthentication(
+        resData.email,
+        resData.localId,
+        resData.idToken,
+        +resData.expiresIn
+      );
+    })
+    )
   }
 
   login(_email: string, _password: string) {
@@ -40,6 +52,17 @@ export class AuthService {
         returnSecureToken: true
       }
     ).pipe(catchError(this.handleError));
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
