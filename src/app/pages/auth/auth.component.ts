6c43d11eb96227db1d930302b26662b67762bf8c
m-tonon <math.tonon@gmail.com>
2023-03-21 06:24:21 -0300

    feat: add login request to firebase and subscribe both observable in auth component

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -32,18 +34,21 @@ export class AuthComponent implements OnInit {
   const email = form.value.email;
   const password = form.value.password;
 
+  let authObs: Observable<AuthResponseData>;
+
   if(this.isLoginMode) {
-    //...
+    authObs = this.authService.login(email, password);
   } else {
-    this.authService.signup(email, password).subscribe(
-      resData => {
-        console.log(resData);
-      }, errorMessage => {
-        console.log(errorMessage);
-        this.error = errorMessage;
+    authObs = this.authService.signup(email, password)
+  }
+  
+  authObs.subscribe({
+    next: (resData) => console.log(resData),
+    error: (errorMessage) => {
+      console.log(errorMessage),
+      this.error = errorMessage}
       }
     )
-  }
   
   form.reset();
  }
--- src/app/pages/auth/auth.service.ts
+++ src/app/pages/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { throwError } from "rxjs";
 
 import {environment} from '../../../../environments/environment';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
@@ -42,6 +42,13 @@ export class AuthService {
   }
 
   login(_email: string, _password: string) {
-    // return this.http.post<AuthResponseData>()
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
+      {
+        email: _email,
+        password: _password,
+        returnSecureToken: true
+      }
+    )
   }
 }
